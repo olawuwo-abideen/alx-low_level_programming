c68aa6e1116eba6796b9e55aead9cd8a4c21b916
Olawuwo Abideen <76418051+olawuwo-abideen@users.noreply.github.com>
2023-02-16 12:48:25 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   * Return: Always 0 (Success)
- *    */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int a;
@@ -11,6 +11,7 @@ int main(void)
 	long long int c;
 	char d;
 	float f;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
