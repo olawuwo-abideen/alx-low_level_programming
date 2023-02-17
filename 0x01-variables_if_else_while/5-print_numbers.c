6a4eb56cfeec58f709b94cbdc55833f552f6c1eb
Olawuwo Abideen <76418051+olawuwo-abideen@users.noreply.github.com>
2023-02-17 20:09:06 +0100

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,18 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
- *  * main - main block
- *   * Description: Print all numbers of base 10, starting from 0.
- *    * Return: 0
- *     */
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * Return: 0
+ */
 int main(void)
 {
-		int i;
+	int i;
 
-			for (i = 0; i < 10; i++)
-					{
-								printf("%i", i);
-									}
-				putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
 
-					return (0);
+	return (0);
 }
