b5339a5f029aec4f2cb0ef4c985d291997d89b4c
Olawuwo Abideen <76418051+olawuwo-abideen@users.noreply.github.com>
2023-02-24 01:31:15 +0100

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,16 @@
 /*
- *  File: 0-isupper.c
+ * File: 0-isupper.c
+ * Auth: olawuwo abideen
  */
 
 #include "main.h"
 
 /**
- *  * _isupper - Checks for uppercase characters.
- *   * @c: The character to be checked.
- *    *
- *     * Return: 1 if character is uppercase, 0 otherwise.
- *      */
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
