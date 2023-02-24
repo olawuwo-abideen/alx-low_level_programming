56653ab756e9ceb329deb717d3c68698cd5dd5a2
Olawuwo Abideen <76418051+olawuwo-abideen@users.noreply.github.com>
2023-02-24 01:33:00 +0100

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,20 +1,18 @@
-/*
- *    File: 0-isupper.c
- */
-
 #include "main.h"
 
 /**
- *     _isupper - Checks for uppercase characters.
- *       @c: The character to be checked.
- *        
- *          Return: 1 if character is uppercase, 0 otherwise.
+ * _isdigit - checks if parameter is a number between 0 to 9.
+ * @c: input number.
+ * Return: 1 if is a number (0 to 9), 0 in other case.
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 48 && c <= 57)
+	{
 		return (1);
-
+	}
 	else
+	{
 		return (0);
+	}
 }
