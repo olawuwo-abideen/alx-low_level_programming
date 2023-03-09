d3d40308be09dd716718d690817d0e88aaa916c2
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-03-08 17:29:51 -0800

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: string
- * Return: the length of a string.
+ *   _strlen_recursion - returns the length of a string.
+ *    @s: string
+ *     Return: the length of a string.
  */
 int _strlen_recursion(char *s)
 {
