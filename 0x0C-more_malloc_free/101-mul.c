792960a129a260ee9f2725af7289dcd3d7c0568d
Olawuwo Abideen <76418051+olawuwo-abideen@users.noreply.github.com>
2023-03-17 00:31:37 +0100

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 #define ERR_MSG "Error"
+
 /**
  * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluate
+ * @s: string to be evaluated
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
@@ -20,9 +22,10 @@ int is_digit(char *s)
 	}
 	return (1);
 }
+
 /**
  * _strlen - returns the length of a string
- * @s: string to evaluat
+ * @s: string to evaluate
  *
  * Return: the length of the string
  */
@@ -36,6 +39,7 @@ int _strlen(char *s)
 	}
 	return (i);
 }
+
 /**
  * errors - handles errors for main
  */
@@ -44,10 +48,12 @@ void errors(void)
 	printf("Error\n");
 	exit(98);
 }
+
 /**
- * main - multiplies two positive numbers1
+ * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
