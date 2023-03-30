89d477598cf383287555153500d080dd5adb856f
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-03-30 06:32:37 -0700

    tasks

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * free_list - frees a list
- * @head: head of the linked list.
- * Return: no return.
+ *   free_list - frees a list
+ *    @head: head of the linked list.
+ *     Return: no return.
  */
 
 void free_list(list_t *head)
