d3671d4ce3e056b74e6539403fc733a64a9e225b
Olawuwo Abideen <76418051+olawuwo-abideen@users.noreply.github.com>
2023-03-31 12:38:20 +0100

    tasks

--- 0x12-singly_linked_lists/101-hello_school.asm
+++ 0x12-singly_linked_lists/101-hello_school.asm
@@ -1,15 +1,12 @@
-	SECTION .data
-msg:	db "Hello, Holberton", 0
-fmt:	db "%s", 10, 0
+101-hello_holberton.asm
 
-	SECTION .text
-	extern printf
-	global main
+
+       global    main
+          extern    printf
 main:
-	mov esi, msg
-	mov edi, fmt
-	mov eax, 0
-	call printf
-
-	mov eax, 0
-	ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
